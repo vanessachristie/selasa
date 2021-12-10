27a1d20702c77602623859657d1d89df12e9cf89
vanessachristie <vchristie@student.ciputa.ac.id>
2021-12-10 10:44:24 +0700

    Add project files.

---
+++ selasa/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input Kalimat".ToUpper());
+            var input = Console.ReadLine().ToUpper();
+            while (input.Length <= 3)
+            {
+                Console.WriteLine("KATA PADA KALIMAT KURANG PANJANG!");
+                Console.Write("Input Kalimat: ");
+                input = Console.ReadLine().ToUpper();
+            }
+            Console.Write("Input Kata".ToUpper());
+            var input2 = Console.ReadLine().ToUpper();
+            while (input2.Length <= 1)
+            {
+                Console.WriteLine("HURUF PADA KATA KURANG PANJANG!");
+                Console.Write("Input Kata: ");
+                input2 = Console.ReadLine().ToUpper();
+            }
+            Console.WriteLine("statistik huruf:".ToUpper());
+
+            char[] kata = input.ToCharArray();
+            var lala = new List<char>();
+            Array.Sort(kata);
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
